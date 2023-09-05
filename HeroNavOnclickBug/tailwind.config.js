6bab050103f5c9d292f6bc4d604f01e35925fbfc
jacks <jacks@MAIN-SYSTEM>
2023-09-05 08:53:01 -0400

    Add project files.

---
+++ HeroNavOnclickBug/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Pages/**/*.razor",
+        "./shared/**/*.razor"
+    ],
+    darkMode: true,
+    theme: {
+        extend: {
+ 
+        },
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: [],
+}
